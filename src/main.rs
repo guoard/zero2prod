a5381362d967175234fbe6e37d82e628999207ca
Ali Afsharzadeh <afsharzadeh8@gmail.com>
2023-05-19 08:05:09 +0330

    refactor: use random port to run integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    run(listener)?.await
 }
