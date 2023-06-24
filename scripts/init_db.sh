37e8adc04cbe9c98b4575a7d0a402f8edf9316c0
Ali Afsharzadeh <afsharzadeh8@gmail.com>
2023-06-24 19:52:40 +0330

    fix: separate redis initialization script to resolve CI

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -3,22 +3,6 @@
 set -x
 set -eo pipefail
 
-RUNNING_CONTAINER=$(docker ps --filter 'name=redis' --format '{{.ID}}')
-if [[ -n $RUNNING_CONTAINER ]]; then
-  echo >&2 "there is a redis container already running, kill it with"
-  echo >&2 "
-  docker kill ${RUNNING_CONTAINER}"
-  exit 1
-fi
-
-docker run \
-    -p "6379:6379" \
-    -d \
-    --name "redis_$(date '+%s')" \
-    redis:7
-
->&2 echo "Redis is ready to go!"
-
 if ! [ -x "$(command -v psql)" ]; then
   echo >&2 "Error: psql is not installed."
   exit 1
