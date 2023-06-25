e0e16a37b0e6ca301606c0cfcd3f97548dc248cd
Ali Afsharzadeh <afsharzadeh8@gmail.com>
2023-06-25 17:52:08 +0330

    fix: update sql data and resolve clippy warnings

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -45,7 +45,7 @@ pub async fn change_password(
                 FlashMessage::error("The current password is incorrect.").send();
                 Ok(see_other("/admin/password"))
             }
-            AuthError::UnexpectedError(_) => Err(e500(e).into()),
+            AuthError::UnexpectedError(_) => Err(e500(e)),
         };
     }
 
