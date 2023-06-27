cb07bf41b82c14f32727c0331b96cf5a8f0d28c1
Ali Afsharzadeh <afsharzadeh8@gmail.com>
2023-06-27 18:58:55 +0330

    Be fault tolerant in mail dispatch workflow

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,6 +1,10 @@
 use crate::helpers::{assert_is_redirect_to, spawn_app, ConfirmationLinks, TestApp};
+use fake::faker::internet::en::SafeEmail;
+use fake::faker::name::en::Name;
+use fake::Fake;
 use std::time::Duration;
 use wiremock::matchers::{any, method};
+use wiremock::MockBuilder;
 use wiremock::{Mock, ResponseTemplate};
 
 #[tokio::test]
@@ -78,7 +82,13 @@ async fn newsletters_are_not_delivered_to_unconfirmed_subscribers() {
 }
 
 async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
-    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let name: String = Name().fake();
+    let email: String = SafeEmail().fake();
+    let body = serde_urlencoded::to_string(&serde_json::json!({
+        "name": name,
+        "email": email
+    }))
+    .unwrap();
 
     let _mock_guard = Mock::given(method("post"))
         .respond_with(ResponseTemplate::new(201))
@@ -117,7 +127,7 @@ async fn newsletter_creation_is_idempotent() {
     app.test_user.login(&app).await;
 
     Mock::given(method("POST"))
-        .respond_with(ResponseTemplate::new(200))
+        .respond_with(ResponseTemplate::new(201))
         .expect(1)
         .mount(&app.email_server)
         .await;
@@ -155,7 +165,7 @@ async fn concurrent_form_submission_is_handled_gracefully() {
     Mock::given(method("POST"))
         // Setting a long delay to ensure that the second request
         // arrives before the first one completes
-        .respond_with(ResponseTemplate::new(200).set_delay(Duration::from_secs(2)))
+        .respond_with(ResponseTemplate::new(201).set_delay(Duration::from_secs(2)))
         .expect(1)
         .mount(&app.email_server)
         .await;
@@ -178,4 +188,54 @@ async fn concurrent_form_submission_is_handled_gracefully() {
     );
 
     // Mock verifies on Drop that we have sent the newsletter email **once**
+}
+
+fn when_sending_an_email() -> MockBuilder {
+    Mock::given(method("POST"))
+}
+
+#[tokio::test]
+async fn transient_errors_do_not_cause_duplicate_deliveries_on_retries() {
+    let app = spawn_app().await;
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "text_content": "Newsletter body as plain text",
+        "html_content": "<p>Newsletter body as HTML</p>",
+        "idempotency_key": uuid::Uuid::new_v4().to_string()
+    });
+    // Two subscribers instead of one!
+    create_confirmed_subscriber(&app).await;
+    create_confirmed_subscriber(&app).await;
+    app.test_user.login(&app).await;
+
+    // Part 1 - Submit newsletter form
+    // Email delivery fails for the second subscriber
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(201))
+        .up_to_n_times(1)
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(500))
+        .up_to_n_times(1)
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+    assert_eq!(response.status().as_u16(), 500);
+
+    // Part 2 - Retry submitting the form
+    // Email delivery will succeed for both subscribers now
+    when_sending_an_email()
+        .respond_with(ResponseTemplate::new(201))
+        .expect(1)
+        .named("Delivery retry")
+        .mount(&app.email_server)
+        .await;
+    let response = app.post_publish_newsletter(&newsletter_request_body).await;
+    assert_eq!(response.status().as_u16(), 303);
+
+    // Mock verifies on Drop that we did not send out duplicates
 }
